5ff43147061414207fc70c6a5e2f86ebec9e6537
mcscope <mcscope@gmail.com>
2017-08-02 18:43:45 -0700

    Make a performance problem by actually calling fortune yourself... sigh

--- microservice.py
+++ microservice.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 """Implements Unix Fortune using 3 microservices and an API"""
+import os
+import os.path
 import sys
 from flask import Flask, request, render_template_string
 import requests
+import random
 
 FETCH_URL = "http://localhost:9001/fetch"
 RENDER_URL = "http://localhost:9002/render"
@@ -20,9 +23,33 @@ def main():
     return requests.post(RENDER_URL, data={'fortunes': fortunes}).content
 
 
+def parse_fortunes(lines):
+    FORTUNE_SEPERATOR = "%\n"
+    fortunes = []
+    current_fortune_lines = []
+    for line in lines:
+        if line == FORTUNE_SEPERATOR:
+            fortunes.append("".join(current_fortune_lines))
+            current_fortune_lines = []
+        else:
+            current_fortune_lines.append(line)
+    return fortunes
+
+served_fortunes = []
+
+
 @app.route("/fetch")
 def fetch():
-    return requests.get("http://mcscope.com/fortune").content
+    fortune_dir = "/usr/local/Cellar/fortune/9708/share/games/fortunes/"
+    os.chdir(fortune_dir)
+    fortune_files = [f for f in os.listdir(".")
+                     if os.path.isfile(f) and not f.endswith('.dat')]
+    all_fortunes = []
+    for path in fortune_files:
+
+        with open(path, 'r') as fortune_file:
+            all_fortunes.extend(parse_fortunes(fortune_file.readlines()))
+    return random.choice(all_fortunes)
 
 
 @app.route("/render", methods=["POST"])
@@ -40,7 +67,7 @@ def render():
         </body>
     </html>
     """
-    return render_template_string(body_template, fortunes_divs=fortune_divs)
+    return render_template_string(body_template, fortune_divs=fortune_divs)
 
 if __name__ == '__main__':
     app.run(port=int(sys.argv[2]), debug=True)
