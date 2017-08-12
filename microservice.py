4a7347e9fb7808351f6cf90c73aab1a51f4fc734
mcscope <mcscope@gmail.com>
2017-08-12 16:27:33 -0700

    Making a combined demo branch for github master

--- microservice.py
+++ microservice.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 """Implements Unix Fortune using 3 microservices and an API"""
-import os
-import os.path
 import sys
 from flask import Flask, request, render_template_string
 import requests
-import random
 
 FETCH_URL = "http://localhost:9001/fetch"
 RENDER_URL = "http://localhost:9002/render"
@@ -23,33 +20,9 @@ def main():
     return requests.post(RENDER_URL, data={'fortunes': fortunes}).content
 
 
-def parse_fortunes(lines):
-    FORTUNE_SEPERATOR = "%\n"
-    fortunes = []
-    current_fortune_lines = []
-    for line in lines:
-        if line == FORTUNE_SEPERATOR:
-            fortunes.append("".join(current_fortune_lines))
-            current_fortune_lines = []
-        else:
-            current_fortune_lines.append(line)
-    return fortunes
-
-served_fortunes = []
-
-
 @app.route("/fetch")
 def fetch():
-    fortune_dir = "/usr/local/Cellar/fortune/9708/share/games/fortunes/"
-    os.chdir(fortune_dir)
-    fortune_files = [f for f in os.listdir(".")
-                     if os.path.isfile(f) and not f.endswith('.dat')]
-    all_fortunes = []
-    for path in fortune_files:
-
-        with open(path, 'r') as fortune_file:
-            all_fortunes.extend(parse_fortunes(fortune_file.readlines()))
-    return random.choice(all_fortunes)
+    return requests.get("http://mcscope.com/fortune").content
 
 
 @app.route("/render", methods=["POST"])
